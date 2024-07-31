abf6d19722b46c5412a0faabd61a68342de533a4
Krunoslav Husak <kruno@husak.me>
2024-07-31 22:04:10 +0200

    Added AI models

--- backend/app/controllers/models_controllers.go
+++ backend/app/controllers/models_controllers.go
@@ -1,7 +1,16 @@
 package controllers
 
-import "github.com/go-raptor/raptor/v2"
+import (
+	"github.com/go-raptor/raptor/v2"
+	"github.com/h00s/linguai/app/services"
+)
 
 type ModelsController struct {
 	raptor.Controller
+
+	Models *services.ModelsService
+}
+
+func (mc *ModelsController) All(c *raptor.Context) error {
+	return c.JSON(mc.Models.All())
 }
--- backend/app/services/models_service.go
+++ backend/app/services/models_service.go
@@ -1,7 +1,35 @@
 package services
 
-import "github.com/go-raptor/raptor/v2"
+import (
+	"github.com/go-raptor/raptor/v2"
+	"github.com/h00s/linguai/app/models"
+)
 
 type ModelsService struct {
 	raptor.Service
+}
+
+func (ts *ModelsService) All() []models.Model {
+	return []models.Model{
+		{
+			Code: "gpt-4o",
+			Name: "OpenAI GPT-4o",
+		},
+		{
+			Code: "gpt-4o-mini",
+			Name: "OpenAI GPT-4o Mini",
+		},
+		{
+			Code: "gpt-35-turbo",
+			Name: "OpenAI GPT-3.5 Turbo",
+		},
+		{
+			Code: "claude-35-sonnet",
+			Name: "Claude 3.5 Sonnet",
+		},
+		{
+			Code: "claude-3-haiku",
+			Name: "Claude 3 Haiku",
+		},
+	}
 }
