7a528826ced7c6fccf72ec2bddda30d334daf138
Krunoslav Husak <kruno@husak.me>
2024-07-31 19:02:32 +0200

    Proper Authorization header check

--- backend/app/middlewares/auth_middleware.go
+++ backend/app/middlewares/auth_middleware.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"strings"
+
 	"github.com/go-raptor/raptor/v2"
 	"github.com/h00s/linguai/app/services"
 )
@@ -12,7 +14,17 @@ type AuthMiddleware struct {
 }
 
 func (am *AuthMiddleware) New(c *raptor.Context) error {
-	if authKey := c.Get("Authorization"); authKey != am.Auth.Key {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.JSONError(raptor.NewErrorUnauthorized("Missing auth header"))
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return c.JSONError(raptor.NewErrorUnauthorized("Invalid auth header"))
+	}
+
+	if authKey := parts[1]; authKey != am.Auth.Key {
 		return c.JSONError(raptor.NewErrorUnauthorized("Invalid auth key"))
 	}
 
